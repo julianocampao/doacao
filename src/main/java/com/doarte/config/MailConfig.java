6299112088ef44bd602464ac4894fc5b9ccf594f
juliano <juliano@DESKTOP-ND16D3U>
2017-11-29 01:42:36 -0200

    dasdsa

---
+++ src/main/java/com/doarte/config/MailConfig.java
@@ -0,0 +1,35 @@
+package com.doarte.config;
+
+
+import java.util.Properties;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+@Configuration
+public class MailConfig {
+	
+	
+	@Bean
+	public JavaMailSender mailSender() {
+		JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+		mailSender.setHost("smtp.sendgrid.net");
+		mailSender.setPort(587);
+		mailSender.setUsername("apikey");
+		mailSender.setPassword("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");
+		
+		Properties props = new Properties();
+		props.put("mail.transport.protocol", "smtp");
+		props.put("mail.smtp.auth", true);
+		props.put("mail.smtp.starttls.enable", true);
+		props.put("mail.debug", false);
+		props.put("mail.smtp.connectiontimeout", 10000); // miliseconds
+
+		mailSender.setJavaMailProperties(props);
+		
+		return mailSender;
+	}
+	
+}
